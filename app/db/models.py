06912373f2b33d5e47ddcc523781fa2793dd45c8
David Ambrozio <davidambroziodev@gmail.com>
2023-11-15 20:37:13 -0300

    create migration templates

--- app/db/models.py
+++ app/db/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, ForeignKey
 from app.db.base import Base
 
 class User(Base):
@@ -6,3 +6,10 @@ class User(Base):
     id = Column('id', Integer, primary_key=True, autoincrement=True)
     username = Column('username', String, nullable=False, unique=True)
     password = Column('password', String, nullable=False)
+
+class Template(Base):
+    __tablename__ = 'templates'
+    id = Column('id', Integer, primary_key=True, autoincrement=True)
+    description = Column('description', String, nullable=False, unique=True)
+    document_id =  Column('document_id', String, nullable=False, unique=True)
+    user_id = Column('user_id', ForeignKey('users.id'), nullable=False)
