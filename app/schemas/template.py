64751ef6c0abeb3b32e82d422cba336e5f1afafe
David Ambrozio <davidambroziodev@gmail.com>
2023-11-15 20:58:03 -0300

    create template schema

---
+++ app/schemas/template.py
@@ -0,0 +1,12 @@
+from app.schemas.base import CustomBaseModel
+from pydantic import field_validator
+
+class Tempalte(CustomBaseModel):
+    id: str
+    description: str
+
+    @field_validator('description')
+    def validate_name(cls, value):
+        if len(value) == 0:
+            raise ValueError("empty description not allowed")
+        return value
