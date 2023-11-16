a479c7f387fe13e4e15603bc4e236053d1eb2559
David Ambrozio <davidambroziodev@gmail.com>
2023-11-16 19:12:21 -0300

    create user use cases tests

--- app/test/conftest.py
+++ app/test/conftest.py
@@ -1,5 +1,9 @@
 import pytest
+from passlib.context import CryptContext
 from app.db.connection import Session
+from app.db.models import User as UserModel
+
+cryptContext = CryptContext(schemes=['sha256_crypt'])
 
 @pytest.fixture()
 def db_session():
@@ -8,3 +12,19 @@ def db_session():
         yield session
     finally:
         session.close()
+
+@pytest.fixture()
+def user_on_db(db_session):
+    user = UserModel(
+        username='username',
+        password=cryptContext.hash('pass#')
+    )
+
+    db_session.add(user)
+    db_session.commit()
+    db_session.refresh(user)
+
+    yield user
+
+    db_session.delete(user)
+    db_session.commit()
