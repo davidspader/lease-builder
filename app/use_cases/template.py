652e5fa9054d23e099f1b31eab91303d4675a61b
David Ambrozio <davidambroziodev@gmail.com>
2023-11-17 17:10:43 -0300

    create delete template use case and tests

--- app/use_cases/template.py
+++ app/use_cases/template.py
@@ -1,4 +1,6 @@
 from sqlalchemy.orm import Session
+from fastapi.exceptions import HTTPException
+from fastapi import status
 from app.db.models import Template as TemplateModel
 from app.schemas.template import Template
 
@@ -14,3 +16,15 @@ class TemplateUseCases:
     def list_templates(self):
         categories = self.db_session.query(TemplateModel).all()
         return categories
+    
+    def delete_template(self, id: int):
+        template = self.db_session.query(TemplateModel).filter_by(id=id).first()
+
+        if not template:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail='Template not found'
+            )
+        
+        self.db_session.delete(template)
+        self.db_session.commit()
