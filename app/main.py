7961f2ff12e68549e0a11f0a0e57b657dda49bd1
David Ambrozio <davidambroziodev@gmail.com>
2023-11-17 18:59:04 -0300

    add template router to main

--- app/main.py
+++ app/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from app.routes.user_routes import router as user_router
+from app.routes.template_routes import router as template_router
 
 app = FastAPI()
 
 app.include_router(user_router)
+app.include_router(template_router)
